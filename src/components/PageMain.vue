d2823aad197854255242b8a4f1e922df92faaf3f
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-24 19:33:09 +0200

    create : PageMain.vue

---
+++ src/components/PageMain.vue
@@ -0,0 +1,82 @@
+<script>
+
+export default {
+
+computed:{
+
+},
+
+components:{
+   
+},
+
+data(){
+
+    return {
+        
+    }
+
+},
+
+ methods:{
+
+ },
+
+}
+
+</script>
+
+<template>
+
+  <!-- 
+    MAIN 
+
+    BADGE SECTIONS 
+    - badje for each player type
+    
+    BEST GAMING COMPANY SECTION
+    - paragraph
+    - img
+    - discover more button
+
+    BEST GAMING ARENA SECTION
+    - streaming
+    - news 
+    - awards
+    - tournament
+    
+    LIVE STREAMING
+    - live streaming carousel
+
+    UPCOMING MATCH SECTION
+    - battle options buttons
+    - battle options slots
+
+    CHOOSE BEST SECTION
+    - best character carousel
+
+    MANAGE BATTLE SECTION
+    - gaming style carousel
+
+    EXPERT TEAM SECTION
+    - experts slots carousel 
+
+    CLIENTS SECTION
+    - clients feedback slot 
+
+    SHOP SECTION
+    - shop products carousel
+
+    NEW ARTICLES SECTION
+    - new articles carousel 
+
+    NEWS LETTER SECTION
+    - form to subsricbe to new letter 
+
+  -->
+
+</template>
+
+<style >
+
+</style>
