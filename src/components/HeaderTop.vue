14f66f4c6c46b413012dcd646b98b404649aed5c
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-24 23:10:52 +0200

    create : HeaderTop.vue

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,50 @@
+<script>
+
+export default {
+
+computed:{
+
+},
+
+components:{
+   
+},
+
+data(){
+
+    return {
+        
+    }
+
+},
+
+ methods:{
+
+ },
+
+}
+
+</script>
+
+<template>
+
+<div>
+
+<!-- 
+    
+    TOP
+    - logo
+    - nav
+    - search input
+    - cart 
+
+-->
+
+</div>    
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
