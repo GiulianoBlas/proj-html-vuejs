9b2c78650d982b4a1ff58330e758ebef496c7c7f
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-24 19:30:17 +0200

    create : PageHeader.vue

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default {
+
+computed:{
+
+},
+
+components:{
+   
+},
+
+data(){
+
+    return {
+        
+    }
+
+},
+
+ methods:{
+
+ },
+
+}
+
+</script>
+
+<template>
+
+  <!-- 
+    HEADER 
+
+    TOP
+    - logo
+    - nav
+    - search input
+    - cart
+
+    BOTTOM 
+    - jumbotron background
+    - links to socials
+    - read more button
+
+  -->
+
+</template>
+
+<style >
+
+</style>
