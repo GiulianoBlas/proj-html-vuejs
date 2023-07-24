7a921b52208f16f5a8c05265a1ce2f7de6528449
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-24 23:11:19 +0200

    create : HeaderBottom.vue

---
+++ src/components/HeaderBottom.vue
@@ -0,0 +1,49 @@
+<script>
+
+export default {
+
+computed:{
+
+},
+
+components:{
+   
+},
+
+data(){
+
+    return {
+        
+    }
+
+},
+
+ methods:{
+
+ },
+
+}
+
+</script>
+
+<template>
+
+<div>
+
+<!-- 
+
+BOTTOM 
+- jumbotron background
+- links to socials
+- read more button
+
+-->
+
+</div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
