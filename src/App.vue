2397efd9d9b0e3cd75e83381f08d676f18f9a9b3
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-24 18:42:07 +0200

    add : comments to define website structure

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,85 @@
 </script>
 
 <template>
- 
+
+  <!-- 
+    HEADER 
+
+    TOP
+    - logo
+    - nav
+    - search input
+    - cart
+
+    BOTTOM 
+    - jumbotron background
+    - links to socials
+    - read more button
+
+  -->
+
+
+  <!-- 
+    MAIN 
+
+    BADGE SECTIONS 
+    - badje for each player type
+    
+    BEST GAMING COMPANY SECTION
+    - paragraph
+    - img
+    - discover more button
+
+    BEST GAMING ARENA SECTION
+    - streaming
+    - news 
+    - awards
+    - tournament
+    
+    LIVE STREAMING
+    - live streaming carousel
+
+    UPCOMING MATCH SECTION
+    - battle options buttons
+    - battle options slots
+
+    CHOOSE BEST SECTION
+    - best character carousel
+
+    MANAGE BATTLE SECTION
+    - gaming style carousel
+
+    EXPERT TEAM SECTION
+    - experts slots carousel 
+
+    CLIENTS SECTION
+    - clients feedback slot 
+
+    SHOP SECTION
+    - shop products carousel
+
+    NEW ARTICLES SECTION
+    - new articles carousel 
+
+    NEWS LETTER SECTION
+    - form to subsricbe to new letter 
+
+  -->
+
+
+  <!-- 
+    FOOTER 
+    
+    TOP
+    - logo
+    - nav
+
+    BOTTOM 
+    - copyright 
+    - links to socials 
+
+  -->
+
 </template>
 
 <style >
