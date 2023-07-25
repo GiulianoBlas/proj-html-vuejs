1382a2f0ae2d1a16a6deda512311cfd07dd0b7a3
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-25 17:54:20 +0200

    add : FooterTop.vue

---
+++ src/components/FooterTop.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default {
+
+computed:{
+
+},
+
+components:{
+   
+},
+
+data(){
+
+    return {
+        
+    }
+
+},
+
+methods:{
+
+},
+
+}
+
+</script>
+
+<template>
+
+    <!-- logo -->
+
+    <!-- nav -->
+
+</template>
+
+<style >
+
+</style>
