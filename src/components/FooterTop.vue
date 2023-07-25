106881de7c074dd28121fd4e84e8cae53a1b2ffa
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-25 20:29:06 +0200

    add : props & dinamic html to FooterTop.vue

--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -6,6 +6,15 @@ computed:{
 
 },
 
+props:{
+
+    pageLogo : String,
+
+    navOptions : Array,
+
+
+},
+
 components:{
    
 },
@@ -28,12 +37,100 @@ methods:{
 
 <template>
 
-    <!-- logo -->
+    <div class="container-center">
 
-    <!-- nav -->
+        <!-- logo -->
+        <div id="footer-logo">
+
+            <img :src=pageLogo alt="">
+
+        </div>
+
+        <!-- nav -->
+
+        <div class="nav-container">
+
+            <nav>
+
+                <ul>
+
+                    <li v-for=" option in navOptions">
+                        
+                        <a href="#">{{ option.name }}</a>
+                    
+                    </li>
+
+                </ul>
+
+            </nav>
+
+        </div>
+
+    </div>
 
 </template>
 
-<style >
+<style lang="scss" scoped >
+
+@use '../assets/scss/mixins.scss'  as * ;
+
+    .container-center{
+        
+        @include container-center;
+
+        @include d-flex-between;
+
+        padding-top: 9.375rem;
+
+        padding-bottom: 3.125rem;
+
+        border-bottom: 2px solid rgba(255, 255, 255, 0.24);
+
+        #footer-logo{
+
+            width: 12.5rem;
+
+            img{
+
+                width: 100%;
+
+            }
+
+        }       
+        
+        .nav-container{
+
+            nav{
+
+
+                ul{
+
+                    list-style: none;
+
+                    @include d-flex-between;
+
+                    li{
+
+                        padding: .625rem;
+
+                        a{
+
+                            @include nav-list-options;
+
+                        }
+
+                    }
+
+                    li:hover a{
+
+                        color: #00AC4D;
+
+                    }
+
+                }
+            }
+        }
+
+    }
 
 </style>
