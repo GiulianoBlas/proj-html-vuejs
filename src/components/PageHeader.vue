c8aba1d58fb8ed422ddf709fb5bf51fc30833fee
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-25 13:13:45 +0200

    add : headerContent to PageHeader.vue

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -14,13 +14,68 @@ components:{
 
     headerTop,
 
-    headerBottom
+    headerBottom,
    
 },
 
 data(){
 
     return {
+
+        headerContent : {
+
+            pageLogo : "../../src/assets/img/menulogo.png",
+
+            navOptions : [
+
+            {   name : "home",
+                type: "button",
+                icon : "../../src/assets/img/image.svg",
+                options :['option 1','option 2','option 3','option 4',]},
+
+            {   name : "pages",
+                type: "button",
+                icon : "../../src/assets/img/image.svg",
+                options :['option 1','option 2','option 3','option 4',]}, 
+                
+            {   name : "tournament",
+                type: "link",},     
+
+            {   name : "shop",
+                type: "button",
+                icon : "../../src/assets/img/image.svg",
+                options :['option 1','option 2','option 3','option 4',]}, 
+                   
+            {   name : "blog",
+                type: "button",
+                icon : "../../src/assets/img/image.svg",
+                options :['option 1','option 2','option 3','option 4',]},   
+                
+            {   name : "contact",
+                type: "link",},
+
+            ],
+
+            searchIcon : "../../src/assets/img/image (1).svg",
+
+            shopIcon : "../../src/assets/img/image (3).svg",
+
+            removeIcon : "../src/assets/img/image (2).svg",
+
+            socialsIcons : [
+
+                '../../src/assets/img/image (4).svg',
+                '.../../src/assets/img/image (5).svg',
+                '../../src/assets/img/image (6).svg',
+                '.../../src/assets/img/image (7).svg'
+
+            ],
+
+            slogan : "are you ready for your next challenge",
+
+            readMore : " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consectetur. Id veritatis rerum ipsa at molestias doloribus, nisi eius quae."
+
+        }
         
     }
 
@@ -41,10 +96,18 @@ data(){
         <div class="header-bg">
 
         <!-- TOP -->
-            <headerTop/>
+            <headerTop
+            :pageLogo="headerContent.pageLogo"
+            :navOptions="headerContent.navOptions"
+            :searchIcon="headerContent.searchIcon"
+            :shopIcon="headerContent.shopIcon"
+            :removeIcon="headerContent.removeIcon"
+            />
 
         <!-- BOTTOM  -->
-            <headerBottom/>
+            <headerBottom
+
+            />
 
         </div>
 
@@ -55,8 +118,11 @@ data(){
 <style lang="scss" scoped>
 
 .header-bg{
+
     background-image: url('../assets/img/banner1.png') ;
+
     background-size: cover;
+
 }
 
 </style>
