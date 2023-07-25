2af94053d3f725457136360eb78b749bc6a366d2
Giuliano Blas <giuliano.blass@gmail.com>
2023-07-25 20:29:34 +0200

    add : props & dinamic html to FooterBottom.vue

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -6,6 +6,16 @@ computed:{
 
 },
 
+props :{
+
+    copyRight : String,
+
+    designer : String,
+
+    socialsIcons : Array,
+
+},
+
 components:{
    
 },
@@ -28,13 +38,145 @@ methods:{
 
 <template>
 
-    <!-- copyright  -->
+    <div class="container-center">
 
-    <!-- links to socials   -->
-  
+        <!-- copyright  -->
+        <div id="credits-container">
+
+            <div>
+
+                <span>copyright &copy;</span>
+
+                <span>{{ copyRight }}</span>
+
+            </div>
+
+            <div>
+
+                <span>designed by </span>
+
+                <a href="" alt="">{{ designer }}</a>
+
+            </div>
+
+        </div>
+
+        <!-- links to socials   -->
+        <div id="socials-links">
+
+            <ul>
+
+                <li v-for="icon in socialsIcons">
+
+                    <a href="">
+
+                        <img :src="icon" alt="">
+
+                    </a>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+   
 
 </template>
 
-<style >
+<style lang="scss" scoped >
+
+@use '../assets/scss/mixins.scss' as * ;
+.container-center{
+
+    @include container-center;
+
+    @include d-flex-between;
+
+    #credits-container{
+
+        @include d-flex-between;
+
+        padding-top: .625rem;
+
+        padding-bottom: .625rem;
+
+        color: white;
+
+        text-transform: capitalize;
+
+        >div{
+
+
+            padding-inline: .625rem ;
+
+            a{
+                text-decoration: none;
+
+                color:#00AC4D ;
+            }
+
+        }
+
+        >div:nth-child(1){
+
+            border-right: 3px solid white
+        }
+
+    }
+
+    #socials-links{
+
+        ul{
+
+            list-style-type: none;
+
+            @include d-flex-between;
+
+            li{
+
+                padding: .625rem;
+
+                a{
+
+                    padding: .9375rem ;
+
+                    background-color: #202046;
+
+                    width: 3.125rem;
+
+                    height: 3.125rem;
+
+                    border-radius: 50%;
+
+                    margin-bottom: .625rem;
+
+                    display: inline-block;
+
+                    img{
+                        
+                        width:100%;
+
+                        filter: invert(100%)
+                        
+                    }
+
+                }
+
+            }
+
+            li:hover > a{
+
+                background-color:#00AC4D;
+
+            }
+
+        }
+
+    }
+
+}
 
 </style>
